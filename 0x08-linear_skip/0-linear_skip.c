f7d3e62ab482159d501e7d28eaf50c41f5c510d9
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2019-01-10 11:59:37 -0800

    added condition for end of list

--- 0x08-linear_skip/0-linear_skip.c
+++ 0x08-linear_skip/0-linear_skip.c
@@ -1,8 +1,15 @@
 #include "search.h"
 
+/**
+ * linear_skip - find the first matched value in a skip list
+ * @list: the skip list to search
+ * @value: value to search for
+ * Return: NULL if value not found otherwise node of matched value
+ */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *c = list;
+	skiplist_t *t = NULL;
 	int skip = 1;
 	char *text1 = "Value checked at index [%lu] = [%d]\n";
 	char *text2 = "Value found between indexes [%lu] and [%lu]\n";
@@ -18,6 +25,14 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 				continue;
 			}
 			printf(text2, c->index, c->express->index);
+			skip = 0;
+		}
+		if (skip == 1)
+		{
+			t = c;
+			while (t->next)
+				t = t->next;
+			printf(text2, c->index, t->index);
 			skip = 0;
 		}
 		printf(text1, c->index, c->n);
